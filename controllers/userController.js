b59b6481876a372c2e51e7b89eaf2e3b3bbe7a23
Kelvin George <kelvin.george@beacon-india.com>
2024-10-16 20:34:02 +0530

    e44 bcrypt

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,36 +1,48 @@
 const USER = require("../models/usermodel");
 const BOOKS = require("../models/books");
 const jwt= require('jsonwebtoken')
+const bcrypt = require('bcrypt');
 
 const doSignup = (req, res, next) => {
-  console.log(req.body);
-  const doc = {
-    name: req.body.fullname,
-    email: req.body.email,
-    password: req.body.password,
-    mob: req.body.mob,
-  };
-  USER(doc)
-    .save()
-    .then((result) => {
-      res.status(200).json("signup successfull");
-    })
-    .catch((err) => {
-      console.log(err);
-      next(err);
-    });
+  const saltRounds = 10;
+  bcrypt.hash(req.body.password,saltRounds).then((hash)=>{
+    console.log(req.body.password,hash);
+    const doc = {
+      name: req.body.fullname,
+      email: req.body.email,
+      password: hash,
+      mob: req.body.mob,
+    };
+    USER(doc)
+      .save()
+      .then((result) => {
+        res.status(200).json("signup successfull");
+      })
+      .catch((err) => {
+        console.log(err);
+        next(err);
+      });
+  })
+  .catch((err)=>{
+    next(err)
+  })
+
+ 
 };
 const doLogin = async (req, res, next) => {
   try {
     const { email, password } = req.body;
     const userData = await USER.findOne({ email: email });
     if (userData) {
-      if (userData.password === password) {
-        const token =jwt.sign({id:userData._id,name:userData.name,email:userData.email},process.env.JWT_PASS,{expiresIn:"1d"})
-        res.status(200).json({ message: "Login  successfull",token:token });
-      } else {
-        res.status(403).json({ message: "invalid credentials" });
-      }
+      bcrypt.compare(password,userData.password).then((match)=>{
+        if (match) {
+          const token =jwt.sign({id:userData._id,name:userData.name,email:userData.email},process.env.JWT_PASS,{expiresIn:"1d"})
+          res.status(200).json({ message: "Login  successfull",token:token });
+        } else {
+          res.status(403).json({ message: "invalid credentials" });
+        }
+      })
+  
     } else {
         res.status(403).json({message:"invalid credentials"})
     }
